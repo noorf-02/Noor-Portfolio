cbf0be109b64cda665c7a4dfc01f4d8a05b7be6c
Noor Fatima <noor2239064@gmail.com>
2026-05-21 23:54:24 +0500

    added transitions

--- src/components/ui/Spage.jsx
+++ src/components/ui/Spage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import { motion } from "framer-motion"
 
 export default function Spage() {
   return (
@@ -64,7 +65,7 @@ export default function Spage() {
 
       <div className="skill-card flex flex-col lg:flex-row justify-center gap-[50px] lg:items-stretch items-center">
         
-        <div className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center">
+        <motion.div initial={{opacit: 0 , x: -50}} animate={{opacity:1, x:0}} className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center">
           <span className="text-pink-500 bg-white rounded-full w-14 h-14 flex items-center justify-center">
               <i class="fa-solid fa-code text-lg"></i>
           </span>
@@ -78,9 +79,9 @@ export default function Spage() {
             <div className="w-[15rem] bg-white rounded-full">
                 <div className="h-full rounded-full bg-pink-100 w-[40%] font-bold text-pink-500 px-2.5 text-center"> 40% </div>
             </div>
-        </div>
+        </motion.div>
 
-        <div className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center gap-3.5">
+        <motion.div initial={{opacity:0 , x:-50}} animate={{opacity:1, x:0}} className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center gap-3.5">
           <span className="text-pink-500 bg-white rounded-full w-14 h-14 flex items-center justify-center">
               <i class="fa-solid fa-ranking-star text-lg"></i>
           </span>
@@ -94,9 +95,9 @@ export default function Spage() {
             <div className="w-[15rem] bg-white rounded-full">
                 <div className="h-full rounded-full bg-pink-100 w-[60%] font-bold text-pink-500 px-2.5 text-center"> 60% </div>
             </div>
-        </div>
+        </motion.div>
 
-        <div className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center">
+        <motion.div initial={{opacity:0, x:-50}} animate={{opacity:1, x:0}} className="card justify-around bg-pink-500 w-[300px] rounded-4xl p-10 flex flex-col items-center">
           <span className="text-pink-500 bg-white rounded-full w-14 h-14 flex items-center justify-center">
               <i class="fa-solid fa-hashtag text-lg"></i>
           </span>
@@ -110,10 +111,11 @@ export default function Spage() {
             <div className="w-[15rem] bg-white rounded-full">
                 <div className="h-full rounded-full bg-pink-100 w-[80%] font-bold text-pink-500 px-2.5 text-center"> 80% </div>
             </div>
-        </div>
+        </motion.div>
         
       </div>
     </div>
+
   );
 }
 
